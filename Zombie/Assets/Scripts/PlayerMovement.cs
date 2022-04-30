a11dd39dc9fd0849f963c92ca842e1760a306750
Seungha Son <linuxias@gmail.com>
2022-04-30 17:42:15 +0900

    Implements player input & movement
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- Zombie/Assets/Scripts/PlayerMovement.cs
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -11,21 +11,38 @@ public class PlayerMovement : MonoBehaviour {
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
 
     private void Start() {
-        // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
     private void FixedUpdate() {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        Rotate();
+        Move();
+
+        // Move 파라미터는 애니메이터의 Movement 상태의 블렌드 트리에서 사용.
+        playerAnimator.SetFloat("Move", playerInput.move);
+
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
     private void Move() {
+        // 이동 거리 계산
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
 
+        // 리지드바디를 이용한 게임 오브젝트 위치 변경
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
     private void Rotate() {
+        // 상대적으로 회전할 수치 계산
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
 
+        // Rigidbody를 이용한 게임 오브젝트 회전 변경
+        playerRigidbody.rotation =
+            playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
     }
 }
